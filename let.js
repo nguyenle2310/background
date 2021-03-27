0e13cb4cfbfd1c6f0dd5882398ec7148b75b6414
Gwen Le <hothaonguyen.le90@gmail.com>
2021-03-27 14:02:42 -0300

    add starting project

---
+++ let.js
@@ -0,0 +1,18 @@
+function printName(){
+    var name = "gwen";
+    console.log(name);
+};
+printName();
+//console.log(name); //not allowed since name is local variable within scope of function
+
+
+// var name ='gwen';
+// if (name == 'gwen') {
+//     fullName ='leGwen';
+//     console.log(fullName);
+// }
+// console.log(fullName);
+
+let name ='gwen';
+let name ='le';
+console.log(name);
